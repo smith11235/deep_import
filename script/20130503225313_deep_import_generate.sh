26713a000b8c198b821f9b872ed0394345e50a4b
smith11235 <smith11235@gmail.com>
2013-05-03 19:47:43 -0400

    removing unnecessary scripts

--- script/20130503225313_deep_import_generate.sh
+++
@@ -1,6 +0,0 @@
-rails generate model SoftProduct soft_id:integer soft_user:references
-rails generate model SoftUser soft_id:integer
-rails generate model SoftReview soft_id:integer soft_product:references
-rails generate model SoftFeature soft_id:integer soft_product:references
-rails generate model SoftAttribute soft_id:integer soft_feature:references
-rails generate model SoftAccount soft_id:integer soft_user:references
