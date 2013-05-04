bf158b40afe41bbfd3278ef68e7ca93f8339ef3d
smith11235 <smith11235@gmail.com>
2013-05-04 00:35:14 -0400

    got model after_initializer logic inserted

--- lib/deep_import/railtie.rb
+++ lib/deep_import/railtie.rb
@@ -5,6 +5,19 @@ module DeepImport
 	class Railtie < Rails::Railtie
 		railtie_name :deep_import
 
+		initializer "deep_import.add_model_creation_after_initialization" do
+  		%w( DummyModel ).each do |model_name|
+				model_class = model_name.classify
+				puts model_class
+  			Kernel.const_get(model_class).class_eval do 
+					after_initialize do |dummy_model|
+						puts "Whoa! just initialized a #{model_name}"
+					end
+				end
+  		end
+			
+		end
+
 		rake_tasks do
 			load "deep_import/deep_import.rake"
 		end
