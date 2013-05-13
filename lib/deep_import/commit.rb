46c57db1c8a0307dcd0585aeeae5cf36159b1f1b
smith11235 <smith11235@gmail.com>
2013-05-12 20:40:19 -0400

    refacctored commit a little

--- lib/deep_import/commit.rb
+++ lib/deep_import/commit.rb
@@ -9,6 +9,40 @@ module DeepImport
 	class Commit
 		def initialize
 			DeepImport::ModelsCache.show_stats
+			import_models
+			set_associations	
+		end
+
+		def set_associations
+			puts "Setting associations:"
+			Config.deep_import_config[:models].each do |model_class,info|
+				info[ :belongs_to ].each do |parent_class|
+					# - get count of records with each distinct deep_import belongs_to id field values
+					puts mysql_association_update_query(model_class,parent_class).green
+					# - ensure uniqueness is the same on actual association fields
+					# clear deep_import_id's everywhere
+					# delete deep_import models
+					puts "  Finished".green
+				end
+			end
+		end
+
+		def mysql_association_update_query(model_class,parent_class)
+			target_table = model_class.to_s.underscore.pluralize
+			target_association_id_field = parent_class.to_s.underscore + "_id"
+			deep_import_target_association_id_field = "deep_import_" + parent_class.to_s.underscore + "_id"
+			association_table = parent_class.to_s.underscore.pluralize
+			deep_import_target_table = "deep_import_#{target_table}"
+
+			mysql_update_logic = "UPDATE #{target_table}"
+			mysql_update_logic << "	JOIN #{deep_import_target_table} ON #{target_table}.deep_import_id = #{deep_import_target_table}.deep_import_id"
+			mysql_update_logic << " JOIN #{association_table} ON #{deep_import_target_table}.#{deep_import_target_association_id_field} = #{association_table}.deep_import_id"
+			mysql_update_logic << " SET #{target_table}.#{target_association_id_field} = #{association_table}.id"
+			mysql_update_logic << " WHERE #{target_table}.deep_import_id IS NOT NULL"
+			mysql_update_logic
+		end
+
+		def import_models
 			cache = DeepImport::ModelsCache.get_cache
 
 			puts "ActiveRecord::import:"
@@ -19,32 +53,6 @@ module DeepImport
 				puts "    - todo: check for failures".red
 				puts "  	Finished".green	
 			end
-
-			puts "Setting associations:"
-			Config.deep_import_config[:models].each do |model_class,info|
-				info[ :belongs_to ].each do |parent_class|
-					target_table = model_class.to_s.underscore.pluralize
-					target_association_id_field = parent_class.to_s.underscore + "_id"
-					deep_import_target_association_id_field = "deep_import_" + parent_class.to_s.underscore + "_id"
-					association_table = parent_class.to_s.underscore.pluralize
-					deep_import_target_table = "deep_import_#{target_table}"
-
-					# - get count of records with each distinct deep_import belongs_to id field values
-
-					update_logic = "UPDATE #{target_table}"
-					update_logic << "	JOIN #{deep_import_target_table} ON #{target_table}.deep_import_id = #{deep_import_target_table}.deep_import_id"
-					update_logic << " JOIN #{association_table} ON #{deep_import_target_table}.#{deep_import_target_association_id_field} = #{association_table}.deep_import_id"
-					update_logic << " SET #{target_table}.#{target_association_id_field} = #{association_table}.id"
-					update_logic << " WHERE #{target_table}.deep_import_id IS NOT NULL"
-					puts update_logic.green
-
-					# - ensure uniqueness is the same on actual association fields
-					# clear deep_import_id's everywhere
-					# delete deep_import models
-					puts "  Finished".green
-				end
-			end
-			
 		end
 
 	end
