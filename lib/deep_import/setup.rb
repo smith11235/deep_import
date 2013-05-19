b17e37b9b74f85f4f960901877716a607c61ae21
smith11235 <smith11235@gmail.com>
2013-05-19 19:24:08 -0400

    - new notes on how i'm doing setup 	- new setup logic to generate migrations mannually

--- lib/deep_import/setup.rb
+++ lib/deep_import/setup.rb
@@ -5,21 +5,49 @@ module DeepImport
 		def initialize
 			@config = Config.deep_import_config
 
-			migrations_for_existing_models
+			enhance_models
+			add_deep_import_models
 
 			puts "- " + "git add .".red + "# to add all new generated migration and model files"
 			puts "- " "rake db:migrate".red
 		end
 
-		def migrations_for_existing_models
+		def enhance_models 
 			@config[:models].each do |model_class,info|
 				plural_name = model_class.to_s.pluralize
 				table_name = plural_name.underscore
+
 				name = "AddDeepImportIdTo#{plural_name}"
+
 				raise "Already a migration named: #{name}, suggest: rm db/migrate/*deep_import*" if Dir.glob( "db/migrate/*_#{name.underscore}.rb" ).size > 0
 				create_migration( name ) do |f|
-    			f.puts "      add_column :#{table_name}, :deep_import_id, :string"
-					f.puts "      add_index :#{table_name}, [:deep_import_id, :id], :name => 'di_id_index'"
+    			f.puts "    add_column :#{table_name}, :deep_import_id, :string"
+					f.puts "    add_index :#{table_name}, [:deep_import_id, :id], :name => 'di_id_index'"
+				end
+			end
+		end
+
+		def add_deep_import_models
+			@config[:models].each do |model_class,info|
+				plural_name = model_class.to_s.pluralize
+				table_name = plural_name.underscore
+
+				name = "CreateDeepImport#{plural_name}"
+
+				raise "Already a migration named: #{name}, suggest: rm db/migrate/*deep_import*" if Dir.glob( "db/migrate/*_#{name.underscore}.rb" ).size > 0
+				create_migration( name ) do |f|
+    			f.puts "    create_table :deep_import_#{table_name} do |t|"
+      		f.puts "      t.string :deep_import_id"
+      		f.puts "      t.datetime :parsed_at"
+      		f.puts "      t.timestamps"
+					info[:belongs_to].each do |belongs_to|
+      			f.puts "      t.string :deep_import_#{belongs_to.to_s.underscore}"
+					end
+					f.puts "    end"
+
+					info[:belongs_to].each do |belongs_to|
+						f.puts "    add_index :deep_import_#{table_name}, [:deep_import_id, :deep_import_#{belongs_to.to_s.underscore}_id], :name => 'di_#{belongs_to.to_s.underscore}'"
+    			end
 				end
 			end
 		end
@@ -35,40 +63,6 @@ module DeepImport
 				f.puts "end"
 			end
 			puts "Generated: #{migration_file}".green
-		end
-
-		def generate_index_migrations
-			@config[:models].each do |model_class,info|
-				plural_name = model_class.to_s.pluralize
-				table_name = plural_name.underscore
-
-				create_migration( "AddDeepImportIdIndexTo#{plural_name}" ) do |f|
-					f.puts "      add_index :#{table_name}, [:deep_import_id, :id], :name => 'di_id_index'"
-				end
-				create_migration( "AddDeepImportBelongsToIndiciesToDeepImport#{model_class.to_s.pluralize}" ) do |f|
-					info[:belongs_to].each do |belongs_to|
-						f.puts "      add_index :deep_import_#{table_name}, [:deep_import_id, :deep_import_#{belongs_to.to_s.underscore}_id], :name => 'di_#{belongs_to.to_s.underscore}'"
-					end
-				end
-			end
-		end
-
-		def write_models_script( method )
-			script_path = File.join Rails.root, "script", "#{@timestamp}_deep_import_#{method}.sh"
-			puts "Writing: #{script_path}".yellow
-			File.open( script_path, "w" ) do |f|
-				@generate_statements.each do |statement|
-					f.puts "rails #{method} #{statement}"
-				end
-			end
-		end
-
-		def new_model_string( name )
-			"model DeepImport#{name} deep_import_id:string parsed_at:datetime"
-		end
-
-		def add_deep_import_id_migration_string( name )
-			"migration AddDeepImportIdTo#{name.to_s.pluralize} deep_import_id:string"
 		end
 
 		def setup_deep_import_generate_statements
