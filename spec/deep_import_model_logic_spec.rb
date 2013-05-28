b2aad5e39d519cc0305c27977fd58a4f5f281d8d
smith11235 <smith11235@gmail.com>
2013-05-27 23:05:20 -0400

    more model logic testing

--- spec/deep_import_model_logic_spec.rb
+++ spec/deep_import_model_logic_spec.rb
@@ -25,23 +25,42 @@ describe "DeepImport::ModelLogic" do
 				model_instance.should be_respond_to :deep_import_after_initialize
 			end
 
+			it "is tracked by model cache" do
+				DeepImport::ModelsCache.cached_instances( model_class ).should be_an_instance_of( Array )
+			end
+		end
+	end
+
+	describe "Model creation tracking" do
+		# only test the root here as a basic sanity check
+		root_class = DeepImport::Config.deep_import_config[:roots][0]
+		root_instance = root_class.new
+		it "should be the last instance in the cache" do
+			DeepImport::ModelsCache.cached_instances( root_class ).last.should be(root_instance)
+		end
+	end
+
+	describe "Model Tracking Blocks" do
+		root_class = DeepImport::Config.deep_import_config[:roots][0]
+		let( :model ) { # create with env var
+		}
+			# cache shouldnt grow
+		let( :model2 ) { # create with deep_import_id => "dummy"
+		}
+			# cache shouldnt grow
+		# save model, find it, cache shouldnt grow
+	end
 =begin
 			- disabled testing:
 				- does not run after_initialize
 					- if env var set
+						- new.save
 					-	if not a new record
+						- find the one we just made
 					-	if deep_import_id is set
+						- create with this set
 					- cache does not grow
 
-			- for root only:
-				- cache is tracking this model
-					- let model.new
-					- DeepImport::ModelsCache.cached_instances( model_class ) #=> array of instances
-					- size = 1
-					- [0] == model_instance
-
 =end
-		end
-	end
 
 end
