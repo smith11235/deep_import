b419aeb3a1ee2d39b89db6fae1e6c8622e5d2d96
smith11235 <smith11235@gmail.com>
2013-07-04 11:13:07 -0400

    better design of model logic

--- lib/deep_import/model_logic.rb
+++ lib/deep_import/model_logic.rb
@@ -1,43 +1,55 @@
 module DeepImport 
 
-	class ModelLogic
-
-		def initialize( model_class )
-			@model_class = model_class
-			add_class_methods
-
-			# call setup logic
-			@model_class.deep_import_setup
+	def self.add_model_logic_to( model_class )
+		# meta-programming reference: http://www.vitarara.org/cms/ruby_metaprogamming_declaratively_adding_methods_to_a_class
+		# on tweaking rails: http://errtheblog.com/posts/18-accessor-missing
+		# on rails model callbacks: http://guides.rubyonrails.org/active_record_validations_callbacks.html#after_initialize-and-after_find
+		model_class.class_eval do
+			# pull in the deep import logic
+			include DeepImport::ModelLogicEnhancements 
 		end
 
-		def add_class_methods
-			@model_class.class_eval do 
-				attr_accessible :deep_import_id # expose the deep import id
+		model_class.setup # run the setup method exposed by ModelLogicEnhancements
 
-				# after each model initialization, run this method
-				# http://guides.rubyonrails.org/active_record_validations_callbacks.html#after_initialize-and-after_find
-				after_initialize :deep_import_after_initialize
+		model_class.class_eval do
+			# todo: remove this
+			def self.parent_class
+				DeepImport::Config.parent_class_of self
+			end
+		end	
 
-				def deep_import_after_initialize
+	end
+
+	module ModelLogicEnhancements
+
+		def self.included(base) # :nodoc:
+			base.extend ClassMethods
+		end
+
+		module ClassMethods
+			def setup
+				send :attr_accessible, :deep_import_id # expose the deep_import_id methods
+
+				setup_after_initialization_callback
+
+			end
+
+			def setup_after_initialization_callback 
+				# add the after initialization callback method
+				send :define_method, :deep_import_after_initialize do
 					# this is called after new and find, we want to check if this really is new
 					return unless self.new_record? # if its a preexisting model
 					return unless self.deep_import_id.nil? # if it already has a deep import id
 					return unless ENV["disable_deep_import"].nil? # if deep_import functionality is disabled
-
+					# add this new instance to the cache
 					DeepImport::ModelsCache.add( self )
 				end
-
-				def self.deep_import_setup
-					DeepImport::ModelsCache.track_model self
-				end
-
-				def self.parent_class
-					DeepImport::Config.parent_class_of self
-				end
-
+				# add a callback to 'deep_import_after_initialize' after each model initialization
+				send :after_initialize, :deep_import_after_initialize
 			end
 
 		end
+
 	end
 
 end
