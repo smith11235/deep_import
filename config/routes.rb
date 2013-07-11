18af5fb193bb419d0180b79a92901a42145a4e72
smith11235 <smith11235@gmail.com>
2013-07-10 21:31:40 -0400

    adding default route

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ DeepImport::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'family#index'
 
   # See how all your routes lay out with "rake routes"
 
