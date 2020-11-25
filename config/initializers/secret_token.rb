6c8d7237ac2ca41e72ceba2005501a5581c36791
Michael Smith <smith11235@gmail.com>
2020-11-25 17:09:57 +0000

    removing rails app code

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DeepImport::Application.config.secret_token = 'd0373d1601f89d29885af8d76920503f9d2ab8fe6554b7846997d5ebee55ee427c7fdf610c415da6d279fa8a76081a271ee45fb4c297e2038c1a36e1d5c2ce28'
